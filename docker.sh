8e397de7e0d17ea8d55abfc40d90df25c84fa6e1
Habasch Parwar, GHR-POD-NEX-18 <Parwar.Habasch@swisscom.com>
2023-08-13 02:54:24 +0200

    feat: added multiple image

--- docker.sh
+++ docker.sh
@@ -1,73 +1,96 @@
 #!/bin/bash
-# =====================================================================
-# This script is used to create a docker container with mariadb image |
-# =====================================================================
+
+MARIADB_IMAGE="docker.io/library/mariadb:latest"
+MYSQL_IMAGE="docker.io/library/mysql:latest"
+POSTGRESQL_IMAGE="docker.io/library/postgres:latest"
+MONGODB="docker.io/library/mongo:latest"
+REDIS="docker.io/library/redis:latest"
+
+
+pullImage() {
+  if ! docker image inspect "$IMAGE" &>/dev/null; then
+    echo "Pulling $IMAGE image..."
+    docker pull "$IMAGE"
+    echo "Image pulled successfully."
+  else
+    echo "Image is already available."
+  fi
+}
+
+readCommonInputs() {
+  read -p "Enter the name of the container: " NAME
+  read -p "Enter the username: " USERNAME
+  read -p "Enter the password: " PASSWORD
+  read -p "Enter the host port: " HOST_PORT
+  read -p "Enter the container port: " CONTAINER_PORT
+}
+
+createContainerWithMariaDB() {
+  pullImage
+  readCommonInputs
+  docker run --name "$NAME" --user "$USERNAME" -e MYSQL_ROOT_PASSWORD="$PASSWORD" -p "$HOST_PORT":"$CONTAINER_PORT" -d "$IMAGE"
+  echo "$NAME is running on port $HOST_PORT:$CONTAINER_PORT with the user $USERNAME and the password $PASSWORD and the image $IMAGE"
+}
+
+dockerYmlFile() {
+  pullImage
+  readCommonInputs
+  echo "version: '3.1'
+
+services:
+  $NAME:
+    image: $IMAGE
+    container_name: $NAME
+    restart: always
+    environment:
+      MYSQL_ROOT_PASSWORD: $PASSWORD
+    ports:
+      - $HOST_PORT:$CONTAINER_PORT
+    volumes:
+      - /var/lib/mysql
+" > docker-compose.yml
+  echo "The yml file is created"
+}
 
 read -p "Choose which task you want to do:
   [1] Create a container
   [2] Create a docker-compose.yml file
   [3] Exit
-" choice
+" CHOICE
 
-createContainer() {
-  echo "Enter values for the container:"
+if [[ $CHOICE == '1' || $CHOICE == '2' ]]; then
+  read -p "Choose which database you want to use:
+  [1] MariaDB
+  [2] MySQL
+  [3] PostgreSQL
+  [4] SQLite
+  [5] MongoDB
+  [6] Redis
+  [7] Exit
+" IMAGE
 
-  read -p "Enter the name of the container:" name
+  if [[ $IMAGE == '7' ]]; then
+    exit 0
+  fi
+  
+else
+  exit 0
 
-  read -p  "Enter the user name:" userName
+  case $IMAGE in
+    1) IMAGE="$MARIADB_IMAGE";;
+    2) IMAGE="$MYSQL_IMAGE";;
+    3) IMAGE="$POSTGRESQL_IMAGE";;
+    4) IMAGE="$SQLITE_IMAGE";;
+    5) IMAGE="$MONGODB";;
+    6) IMAGE="$REDIS";;
+    7) exit 0;;
+    *) echo "Invalid choice"; exit 1;;
+  esac
+fi
 
-  read -p "Enter the password: " password
-
-  read -p "Enter the host port: " hostPort
-
-  read -p "Enter the container port: " containerPort
-
-  image="docker.io/library/mariadb:latest"
-
-docker run --name $name --user $userName -e MYSQL_ROOT_PASSWORD=$password -p $hostPort:$containerPort -d $image
-
-echo "$name is running on port $hostPort:$containerPort with the user $userName and the password $password and the image $image"
-}
-
-dockerYmlFile() {
-
-  echo "Enter values for the docker-compose.yml file:"
-
-  read -p "Enter the name of the container:" name
-
-  read -p  "Enter the user name:" userName
-
-  read -p "Enter the password: " password
-
-  read -p "Enter the host port: " hostPort
-
-  read -p "Enter the container port: " containerPort
-
-  image="docker.io/library/mariadb:latest"
-
-echo "version: '3.1'
-
-services:
-  $name:
-    image: $image
-    container_name: $name
-    restart: always
-    environment:
-      MYSQL_ROOT_PASSWORD: $password
-    ports:
-      - $hostPort:$containerPort
-    volumes:
-      - /var/lib/mysql
-" > docker-compose.yml
-
-echo "The yml file is created"
-}
-
-
-case $choice in
-  1) createContainer;;
+case $CHOICE in
+  1) createContainerWithMariaDB;;
   2) dockerYmlFile;;
   3) exit 0;;
-  *) echo "Invalid choice";;
+  *) echo "Invalid choice"; exit 1;
 esac
-
